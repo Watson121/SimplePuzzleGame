583da665d6019706badb39b0e93de49d87ab8768
Adam Tedder <33036071+Watson121@users.noreply.github.com>
2025-01-24 23:24:03 +0000

    Initial Commit

---
+++ Source/SimplePuzzleGame/SimplePuzzleGame.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimplePuzzleGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimplePuzzleGame, "SimplePuzzleGame" );
+
+DEFINE_LOG_CATEGORY(LogSimplePuzzleGame)
+ 
---
+++ Source/SimplePuzzleGame/SimplePuzzleGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimplePuzzleGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASimplePuzzleGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASimplePuzzleGameGameMode();
+};
+
+
+
