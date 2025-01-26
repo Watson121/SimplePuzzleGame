705f872b8b56a2f08abe61388fab5e58e4c0d1d9
Adam Tedder <33036071+Watson121@users.noreply.github.com>
2025-01-26 00:32:28 +0000

    Added in Key Pad

--- Source/SimplePuzzleGame/Private/Core/SimplePuzzleGamePlayerController.cpp
+++ Source/SimplePuzzleGame/Private/Core/SimplePuzzleGamePlayerController.cpp
@@ -1,7 +1,8 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "Interfaces/InteractionInterface.h"
+
 #include "Core/SimplePuzzleGamePlayerController.h"
+#include "Interfaces/InteractionInterface.h"
 #include "GameFramework/Pawn.h"
 #include "Blueprint/AIBlueprintHelperLibrary.h"
 #include "NiagaraSystem.h"
---
+++ Source/SimplePuzzleGame/Private/KeyPad.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KeyPad.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "KeyCodeNumber.h"
+
+void UKeyPad::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	ConfirmButton->OnClicked.AddUniqueDynamic(this, &UKeyPad::CheckIfCorrect);
+}
+
+void UKeyPad::CheckIfCorrect()
+{
+	FString currentCode;
+
+	currentCode = FString::FromInt(KeyCodeNumberOne->GetValue()) 
+		+ FString::FromInt(KeyCodeNumberTwo->GetValue()) 
+		+ FString::FromInt(KeyCodeNumberThree->GetValue())
+		+ FString::FromInt(KeyCodeNumberFour->GetValue());
+
+	UE_LOG(LogTemp, Warning, TEXT("Current Value is %s"), *currentCode);
+
+	if (currentCode == correctCode) {
+		UE_LOG(LogTemp, Warning, TEXT("Code is correct"));
+		ConfirmationText->SetText(FText::FromString("Code is correct! Well Done!"));
+	}
+	else {
+		ConfirmationText->SetText(FText::FromString("Code is incorrect! Try Again!"));
+	}
+
+}
+
+// Generate a brand new code
+void UKeyPad::GenerateCode()
+{
+	int newCode = FMath::RandRange(1000, 9999);
+	correctCode = FString::FromInt(newCode);
+
+	UE_LOG(LogTemp, Warning, TEXT("Correct Code: %s"), *correctCode);
+}
